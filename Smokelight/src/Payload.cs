8b62cb4941fa45e6227ff7ce9350e78623596a56
Woohyun Shin <cpu344@gmail.com>
2025-12-19 14:15:45 +0900

    Revert unnecessary check

--- Smokelight/src/Payload.cs
+++ Smokelight/src/Payload.cs
@@ -39,11 +39,7 @@ public class Payload : IEquatable<Payload> {
         this.name = name;
         this.type = type;
         this.data = data;
-        try {
-            textData = type == PayloadType.Text ? Encoding.UTF8.GetString(data) : null;
-        } catch (ArgumentException e) {
-            throw new InvalidDataException("Text payload contains an illegal UTF-8 sequence", e);
-        }
+        textData = type == PayloadType.Text ? Encoding.UTF8.GetString(data) : null;
     }
 
     /*
